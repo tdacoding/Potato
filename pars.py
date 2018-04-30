daa5a36a97371b73452d6472556618532116e4fd
tdacoding <obafgk1k2m>
2018-04-30 15:02:41 +0300

    pars.py update

--- pars.py
+++ pars.py
@@ -4,15 +4,17 @@ import pandas as pd
 from io import StringIO
 
 flen = []
+fields_name = []
 db = {}
 archive = zipfile.ZipFile('wr3288a4.zip', 'r')
 fields = archive.read('fld3288a4.txt').decode('cp1251').split('\r\n')
 fields.remove('')
 for line in fields:
      flen.append([int(line[10:13].strip()[0]), line[15:].strip()])
-     db[line[15:].strip()]=[]
+     db[line[15:].strip()] = []
+     fields_name.append(line[15:].strip())
 
-print(flen)
+print(fields_name)
 print(db)
 
 data = archive.read('wr3288a4.txt').decode('cp1251').split('\r\n')
@@ -20,9 +22,11 @@ data.remove('')
 for line in data:
     p = 0
     for i in range(len(db)):
-        db[flen[i][1]].append(line[p:p+flen[i][0]])
+        db[flen[i][1]].append(line[p:p+flen[i][0]].strip())
         p += flen[i][0] + 1
-print(flen[1][1])
+#print(flen[1][1])
 
-print(db[flen[28][1]][-10:150969])
-
+#print(db[flen[25][1]][-10:150969])
+d = pd.DataFrame.from_dict(db)
+d = d[fields_name]
+d.to_csv('example.csv', encoding='cp1251')
