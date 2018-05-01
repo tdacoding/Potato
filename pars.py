fb899673c89de17bfe15996f2729e7fe231a870b
tdacoding <obafgk1k2m>
2018-05-02 01:11:00 +0300

    temp.py update

--- pars.py
+++ pars.py
@@ -17,16 +17,15 @@ for line in fields:
 print(fields_name)
 print(db)
 
-data = archive.read('wr3288a4.txt').decode('cp1251').split('\r\n')
-data.remove('')
-for line in data:
-    p = 0
-    for i in range(len(db)):
-        db[flen[i][1]].append(line[p:p+flen[i][0]].strip())
-        p += flen[i][0] + 1
-#print(flen[1][1])
-
-#print(db[flen[25][1]][-10:150969])
-d = pd.DataFrame.from_dict(db)
-d = d[fields_name]
-d.to_csv('example.csv', encoding='cp1251')
+# data = archive.read('wr3288a4.txt').decode('cp1251').split('\r\n')
+# data.remove('')
+# for line in data:
+#     p = 0
+#     for i in range(len(db)):
+#         db[flen[i][1]].append(line[p:p+flen[i][0]].strip())
+#         p += flen[i][0] + 1
+#
+# d = pd.DataFrame.from_dict(db)
+# d = d[fields_name]
+# d.to_csv('Sykt_tepm.csv', encoding='cp1251')
+#print([fields_name[0]]+fields_name[5:10]+fields_name[15:17]+fields_name[18:20]+[fields_name[21]]+fields_name[24:26]+fields_name[27:29])
