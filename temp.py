ef154291c53d55bf4717db6855bd5c362ff8dd36
tdacoding <obafgk1k2m>
2018-05-02 18:06:38 +0300

    temp.py update

--- temp.py
+++ temp.py
@@ -3,14 +3,15 @@ import csv
 import numpy as np
 import pandas as pd
 
-with open('example.csv') as csvfile:
+with open('example.csv', encoding='cp1251') as csvfile:
     spamreader = csv.reader(csvfile)
     fields_name = spamreader.__next__()[1:]
-fields = [fields_name[0]]+fields_name[5:10]+fields_name[15:17]+fields_name[18:20]+[fields_name[21]]+fields_name[24:26]+fields_name[27:29]
-print(fields)
+fields = [fields_name[0]]+fields_name[5:10]+fields_name[15:17]\
+                        +fields_name[18:20]+[fields_name[21]]+fields_name[24:26]+fields_name[27:29]
+
 #     for row in spamreader:
 #         print(row)
 db = pd.read_csv('example.csv', encoding='cp1251')
 
-db = db[fields]
+db['time'] = db[fields_name[5]]+'-'+db[fields_name[6]]
 print(db)
