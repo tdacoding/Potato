72cfb1306e31ec1b9c4b8b43ac339fa674a4735c
tdacoding <timushev@gmail.com>
2018-05-04 15:49:37 +0300

    Work

--- temp.py
+++ temp.py
@@ -2,16 +2,23 @@
 import csv
 import numpy as np
 import pandas as pd
+import datetime as dt
 
-with open('example.csv', encoding='cp1251') as csvfile:
+with open('Sykt_temp_data.csv', encoding='cp1251') as csvfile:
     spamreader = csv.reader(csvfile)
     fields_name = spamreader.__next__()[1:]
-fields = [fields_name[0]]+fields_name[5:10]+fields_name[15:17]\
-                        +fields_name[18:20]+[fields_name[21]]+fields_name[24:26]+fields_name[27:29]
-
+fields = [*fields_name[5:10],*fields_name[15:17],*fields_name[18:20],fields_name[21],*fields_name[24:26],*fields_name[27:29]]
+print(repr(fields))
 #     for row in spamreader:
 #         print(row)
-db = pd.read_csv('example.csv', encoding='cp1251')
+db = pd.read_csv('Sykt_temp_data.csv', encoding='cp1251')
+db = db[fields]
+print(db.loc[0])
+db['time'] = db.apply(lambda row: str(int(row[fields[0]])) + ':' + str(int(row[fields[1]]))+ ':' + str(int(row[fields[2]])) + ':' + str(int(row[fields[3]])) + ":00:00", axis=1) #+'-'+db[fields_name[6]].str
+db['time'] = pd.to_datetime(db['time'], format='%Y:%m:%d:%H:%M:%S')
 
-#db['time'] = db[fields_name[5]].str+'-'+db[fields_name[6]].str
-print(db.loc[1:3])
+db = db.set_index(pd.DatetimeIndex(db['time']))
+print(db.loc['1966-01-02 03:00:00'])
+
+db.to_csv('Sykt_temp_data_cleaned.csv', encoding='cp1251')
+
