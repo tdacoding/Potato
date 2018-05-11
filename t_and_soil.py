25459d73138f09125997ac1b6c99d12562e75719
tdacoding <timushev@gmail.com>
2018-05-11 16:17:32 +0300

    Work

--- t_and_soil.py
+++ t_and_soil.py
@@ -5,9 +5,9 @@ import datetime as dt
 
 
 db1 = pd.read_csv('Sykt_t_data_day_mean.csv', encoding='cp1251')
-#db1 = db1.set_index(pd.DatetimeIndex(db1['time']))
+#db1 = db1.set_index(pd.DatetimeIndex(db1['date']))
 db2 = pd.read_csv('Sykt_Soil_t_data.csv', encoding='cp1251')
-#db2 = db2.set_index(pd.DatetimeIndex(db2['time']))
+#db2 = db2.set_index(pd.DatetimeIndex(db2['date']))
 #print(db2[(~db2.index.isin(db1.index))])
 print(db1.head(0))
 print(db2.head(0))
